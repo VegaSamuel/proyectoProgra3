262eb9a25512495fbd6779c245b3be41ea18ccb8
VegaSamuel <125225079+VegaSamuel@users.noreply.github.com>
2023-11-14 19:43:13 -0700

    Proyecto
    Hecho por el Yisus

---
+++ Proyecto/src/back/Usuario.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package back;
+
+/**
+ *
+ * @author 
+ */
+public class Usuario {
+    String nombre;
+    String correo;
+    String usuario;
+    String contraseña;
+
+    public Usuario(String nombre, String correo, String usuario, String contraseña) {
+        this.nombre = nombre;
+        this.correo = correo;
+        this.usuario = usuario;
+        this.contraseña = contraseña;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+    
+    
+}
